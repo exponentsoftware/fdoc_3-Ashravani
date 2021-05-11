a8ddeaed5e55d42984e5d4aff716334f23666509
Ashravani <ashravani94@gmail.com>
2021-05-11 13:26:49 +0530

    Initial commit

---
+++ student.js
@@ -0,0 +1,7 @@
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const [Name,skills,scores]=student;
+console.log(Name);
+console.log(skills);
+console.log(scores);
+console.log(skills[2],":",scores[2]);
+console.log(skills[3],":",scores[3]);
